74cbb571d1d38535ee0dc05cf3f699d090dd60c6
Alex <alex0195@gmail.com>
2021-08-11 16:56:54 -0400

    priemr comit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const runcl = require('runcl');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, access-token, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+app.get('/:run', function (req, res) {
+    res.send(runcl.run(req.params.run));
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
