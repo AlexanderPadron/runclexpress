1dcac786d70ea4b569720e12db9a300edcfb4a30
Alex <alex0195@gmail.com>
2021-11-16 09:33:12 -0300

    nueva funcionalidad

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const runcl = require('runcl');
+const os = require('os');
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, access-token, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -13,7 +14,8 @@ app.get('/', function (req, res) {
   res.send({
     mensaje: 'Hola mundo',
     status: 'ok',
-    version: '1.0.1'
+    version: '1.0.2',
+    hostname: os.hostname()
   });
 });
 
