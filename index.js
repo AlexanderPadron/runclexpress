c14455fe59373f19b1b31104c063340dcc0160a8
Alex <alex0195@gmail.com>
2021-12-30 16:52:17 -0300

    deploy 104

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const runcl = require('runcl');
 const os = require('os');
+const port = 8080
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, access-token, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -14,7 +15,7 @@ app.get('/', function (req, res) {
   res.send({
     mensaje: 'Hola mundo',
     status: 'ok',
-    version: '1.0.3',
+    version: '1.0.4',
     hostname: os.hostname()
   });
 });
@@ -23,6 +24,6 @@ app.get('/:run', function (req, res) {
     res.send(runcl.run(req.params.run));
 });
 
-app.listen(8080, function () {
-  console.log('Aplicacion que transforma los ruts!');
+app.listen(port || 8080, function () {
+  console.log(`Aplicacion que transforma los ruts, listen on port => ${port}`);
 });
